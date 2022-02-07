f9a9ab898324696aa91bdeb9d9a6f883484fe6d8
diegodelaFuenteCuraqueo <diego.delafuente@ug.uchile.cl>
2022-02-07 13:05:17 -0300

    testing

--- src/Utils/debugCastEvents.js
+++ src/Utils/debugCastEvents.js
@@ -3,9 +3,8 @@ const castDebugLogger = cast.debug.CastDebugLogger.getInstance()
 
 const debugCastEvents = (event) => {
   castDebugLogger.warn("REACT", "### CORE ### : " + JSON.stringify(event))
-  if(event.type === "REQUEST_LOAD"){
-    console.log("REQ LOAD")
-  }
+  castDebugLogger.warn(event.type+" "+event.senderId)
+  cast.framework.sendCustomMessage("urn:x-cast:com.example.cast.mynamespace", event.senderId, event.type)
 }
 
 export default debugCastEvents
